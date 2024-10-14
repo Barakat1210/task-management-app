37a75d67f9d78d20103b57820a3614dc0208703c
Barakat <barakat1102001@gmail.om>
2024-10-15 02:29:43 +0300

    + initialize app + home screen + tasks tab + settings_tab

---
+++ lib/my_app/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/config/theme/my_theme.dart';
+import 'package:todo_app/core/routes_manager.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute:RoutesManager.router ,
+      initialRoute:RoutesManager.homeRoute ,
+      theme:MyTheme.lightTheme ,
+      themeMode:ThemeMode.light ,
+    );
+  }
+}
